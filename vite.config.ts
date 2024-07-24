694391a44ecfe58b9b3a9b3890c1270de868ae6d
vandeptrai <van.d.pham@tgl.sol.com>
2024-07-24 17:17:29 +0700

    "v10"

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,86 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {}
-  },
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          cdnScript: '<script src="https://unpkg.com/react@18"></script>',
+        },
+      },
+    }),
+  ],
   build: {
     minify: false,
     lib: {
-      entry: "./src/main.tsx",
+      entry: "./src/index.js",
       name: "WoodDeck",
-      fileName: (format) => `woodDeck9.${format}.js`,
+      fileName: (format) => `woodDeck10.${format}.js`,
     },
+    // rollupOptions: {
+    //   external: ["react", "react-dom"],
+    //   output: {
+    //     // globals: {
+    //     //   react: 'React',
+    //     //   'react-dom': 'ReactDOM',
+    //     // },
+    //     paths: {
+    //       react: 'https://unpkg.com/browse/react@18.0.0/umd/react.production.min.js',
+    //       'react-dom':'https://unpkg.com/browse/react-dom@18.0.0/umd/react-dom.production.min.js'
+    //     }
+    //   }
+    // }
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
-        // globals: {
-        //   react: 'React',
-        //   'react-dom': 'ReactDOM',
-        // },
-        paths: {
-          react: 'https://unpkg.com/browse/react@18.0.0/umd/react.production.min.js',
-          'react-dom':'https://unpkg.com/browse/react-dom@18.0.0/umd/react-dom.production.min.js'
-        }
-      }
-    }
+        format: "umd",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
   },
 });
+
+// // vite.config.js
+// import { fileURLToPath, URL } from "node:url";
+
+// import vue from "@vitejs/plugin-vue";
+// import { createHtmlPlugin } from "vite-plugin-html";
+
+// export default {
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//     },
+//   },
+//   plugins: [
+//     vue(),
+//     createHtmlPlugin({
+//       inject: {
+//         data: {
+//           cdnScript:
+//             process.env.NODE_ENV === "production"
+//               ? '<script src="https://unpkg.com/vue@3"></script>'
+//               : "",
+//         },
+//       },
+//     }),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       external: ["vue"],
+//       output: {
+//         format: "iife",
+//         globals: {
+//           vue: "Vue",
+//         },
+//       },
+//     },
+//   },
+// };
