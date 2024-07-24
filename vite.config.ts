481994a4240635de7b6191402b093e1feb5b6902
vandeptrai <van.d.pham@tgl.sol.com>
2024-07-24 15:34:29 +0700

    "v5"

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
   build: {
     minify: false,
     lib: {
       entry: "./src/index.js",
       name: "WoodDeck",
-      fileName: (format) => `woodDeck4.${format}.js`,
+      fileName: (format) => `woodDeck5.${format}.js`,
     },
-    rollupOptions: {
-      external: ["react", "react-dom", "three", "@react-three/drei", "@react-three/fiber"],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          three: "Three",
-          "@react-three/drei": "Drei",
-          "@react-three/fiber": "Fiber"
-        }
-      }
-    }
+    // rollupOptions: {
+    //   external: ["react", "react-dom", "three", "@react-three/drei", "@react-three/fiber"],
+    //   output: {
+    //     globals: {
+    //       react: 'React',
+    //       'react-dom': 'ReactDOM',
+    //       three: "Three",
+    //       "@react-three/drei": "Drei",
+    //       "@react-three/fiber": "Fiber"
+    //     }
+    //   }
+    // }
   },
 });
